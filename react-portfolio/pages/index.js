0cf36ea8a0c6268228cee6c059b75f31d09d5239
timothyalton <timothy.alton@gmail.com>
2023-02-23 16:34:29 -0600

    added downloadable button for pdf resume. Added links for linkedin, youtube, and github

--- react-portfolio/pages/index.js
+++ react-portfolio/pages/index.js
@@ -1,12 +1,37 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import {BsFillMoonStarsFill} from 'react-icons/bs';
 import { AiFillLinkedin, AiFillBook, AiFillYoutube, AiFillGithub } from 'react-icons/ai';
 import Image from "next/image";
 import malton from "../public/malton_logo.png";
 import design from "../public/design.png";
 import fullstack from "../public/website_design.png";
+// import resume from "../public/resume_malton.pdf";
 
 export default function Home() {
+
+  const [isHovering, setIsHovering] = useState('Resume');
+
+  const onButtonClick = () => {
+    fetch('resume_malton.pdf').then(res => {
+      res.blob().then(blob => {
+        const fileURL = window.URL.createObjectURL(blob);
+        let alink = document.createElement('a');
+        alink.href = fileURL;
+        alink.download = 'resume_malton.pdf';
+        alink.click();
+      })
+    })
+  }
+
+  const handleMouseOver = () => {
+    setIsHovering('Download')
+  }
+
+  const handleMouseOut = () => {
+    setIsHovering('Resume')
+  }
+
   return (
     <>
       <Head>
@@ -15,15 +40,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=' bg-white px-10'>
+      <main className=' bg-white px-10 font-burtons'>
         <section className="min-h-screen">
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl font-burtons'>Mitchell Alton</h1>
+            <h1 className='text-xl'>Mitchell Alton</h1>
             <ul className='flex items-center'>
               <li>
                 <BsFillMoonStarsFill className='cursor-pointer text-2xl'/>
               </li>
-              <li><a className='bg-gradient-to-r from-cyan-500 to-black bg-cyan-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a></li>
+              <li><button onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} onClick={onButtonClick} className='bg-gradient-to-r from-cyan-500 to-black bg-cyan-500 text-white px-4 py-2 rounded-md ml-8'>{isHovering}</button></li>
             </ul>
           </nav>
           <div className='text-center p-10'>
@@ -37,9 +62,9 @@ export default function Home() {
             </p>
           </div>
           <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
-            <AiFillLinkedin />
-            <AiFillYoutube />
-            <AiFillGithub />
+            <a href="https://www.linkedin.com/IN/mitchell-alton"><AiFillLinkedin /></a>
+            <a href="https://www.youtube.com/@mitchellalton402"><AiFillYoutube /></a>
+            <a href="https://github.com/timothyalton"><AiFillGithub /></a>
           </div>
           <div className='relative mx-auto rounded-full w-80 h-80 mt-20'>
             <Image className="rounded-full w-80 h-80" src={malton} fill="true" style="cover"/>
@@ -47,7 +72,7 @@ export default function Home() {
         </section>
 
         <section>
-          <div>
+          <div className=' text-center'>
             <h3 className="text-3xl py-1">Technical Skills & Services</h3>
             <p className='text-md py-2 leading-8 text-gray-800'>
               I have learned a wide variety of technical skills along my journey into the Tech world. 
@@ -55,7 +80,7 @@ export default function Home() {
             </p>
           </div>
           <div>
-            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+            <div className= 'text-center shadow-lg p-10 rounded-xl my-10'>
               {/* <Image src={design} width={100} height={100} /> */}
               <h3 className='text-lg font-medium pt-8 pb-2'>Website Design</h3>
               <p className='py-2'>
